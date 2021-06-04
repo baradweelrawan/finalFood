5c8ddefc5a48de56f91b3ed36eb8412cf69ba5d2
baradweelrawan <bardaweel_rawan@yahoo.com>
2021-06-04 16:23:57 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,26 +1,37 @@
 'use strict'
-let divEl=document.getElementById('continer');
+// let divEl=document.getElementById('continer');
+let table = document.getElementById('myTable');
 
 let foods=[];
-let priceFood=[];
-let images=['pizza.jpg' ,'shawarma.jpg','thumbnail (1).jpg']
+// let images=['pizza.jpg' ,'shawarma.jpg','thumbnail (1).jpg']
 
 function Foodslover(customerName,foodType){
     this.customerName=customerName;
     this.foodType=foodType;
     // this.imagesName = images.split('.')[0];
     // this.source='images' +imagesName ;
-    this.priceFood=[]
-    foods.push(this)
+    // this.priceFood=[]
+    this.img = `images/${foodType}.jpg`
+    foods.push(this);
 }
 
-function getRandom() {
-    for(let i=0 ; i<foods.length ;i++){
-       this.priceFood.push(Math.random());
+myForm.addEventListener('submit',handleSubmit);
 
-    }
-     
- }
+function handleSubmit(event){
+    event.preventDefault();
+    let customerName=event.target.customerName.value;
+    let foodType=event.target.foodType.value;
+    new Foodslover(customerName,foodType);
+
+    settingItems();
+    render();
+}
+
+
+function getRandom() {
+    return Math.random();
+    
+}
 
  function settingItems(){
    
@@ -33,48 +44,49 @@ function getRandom() {
     console.log(stringObj);
     console.log(typeof stringObj);
     let normalObj = JSON.parse(stringObj );
+    console.log(normalObj);
     if (normalObj !== null){
         foods = normalObj ;
   }
+  console.log(foods);
+  render();
   
   
 }
 
 function render(){
     let tableEl=document.createElement('table');
-    divEl.appendChild(tableEl);
+    table.appendChild(tableEl);
 
-    let trEl=document.createElement('tr');
-    tableEl.appendChild(trEl);
-
-    let tdEl=document.createElement('td');
-    trEl.appendChild(tdEl);
     for (let i =0 ; i<foods.length ; i++){
+        tableEl.textContent='';
+
+        let trEl=document.createElement('tr');
+        tableEl.appendChild(trEl);
+
         let tdEl=document.createElement('td');
-        tdEl.setAttribute('src',foods[i].source);
         trEl.appendChild(tdEl);
-    }
 
-    for (let j=0; j<foods.length ; j++){
+        let imgEl=document.createElement('img');
+        tdEl.appendChild(imgEl);
+        imgEl.setAttribute('src',foods[i].img);
+
         let td1El=document.createElement('td');
-        td1El.textContent(`customerName:${customerName[j]}: ,foodType:${foodType[j]} `)
-        trEl.appendChild(tdEl);
-} 
-}
-render();
+        trEl.appendChild(td1El);
 
+        let pEl=document.createElement('p');
+        td1El.appendChild(pEl);
+        pEl.textContent=`Customer Name:${foods[i].customerName}`;
 
-function handleSubmit(event){
-    event.preventDefault();
-    let customerName=event.target.customerName.value;
-    let foodType=event.target.foodType.value;
-    new Foodslover(customerName,foodType);
+        let p1El=document.createElement('p');
+        td1El.appendChild(p1El);
+        p1El.textContent=`Food Type:${foods[i].foodType}`;
 
-    settingItems();
-    // render();
-}
+        let p2El=document.createElement('p');
+        td1El.appendChild(p2El);
+        pEl.textContent=`Food Price:${getRandom()}`;
+    }
+}   
 
-
-
+  
 gettingItems();
-myForm.addEventListener('submit',handleSubmit);
